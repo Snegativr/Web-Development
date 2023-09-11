19bac8ef95fe0fb0fe786d66588400f50d04aa27
Snegativr <93019177+Snegativr@users.noreply.github.com>
2023-09-11 13:20:38 +0300

    Add files via upload

---
+++ src/components/Body.jsx
@@ -0,0 +1,9 @@
+function Body() {
+    return (
+        <h2>
+            Hello World2!
+        </h2>
+    )
+};
+
+export default Body;
---
+++ src/components/Footer.jsx
@@ -0,0 +1,8 @@
+function Footer() {
+    return (
+        <h3>
+            Hello World3!
+        </h3>
+    )
+};
+export default Footer;
